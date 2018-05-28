7886ba8e1c6a56fbbae7001f29708e4fa200d567
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-05-27 20:08:56 -0500

    wrote functionality of count button

--- solution.rb
+++ solution.rb
@@ -1,25 +1,9 @@
 require 'sinatra'
 require 'pry'
 
-get '/' do 
-
-    def initialize 
-        if !!defined? @sum == false 
-        @sum = 0
-        else 
-        @sum = @sum + 1
-        end 
-    end 
-    erb :index 
-     
-end 
 
 
-
-post '/test' do
-     binding.pry
-    
-    @sum = params[:sum].to_i + 1
+get '/' do    
+    @sum = params[:sum].to_i
     erb :index
 end
-
